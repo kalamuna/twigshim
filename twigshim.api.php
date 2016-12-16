aa6d3d3665f739a6de8ad1d0ca7ab2498f52a98a
Derek DeRaps <derek@kalamuna.com>
2016-12-16 01:53:16 +0000

    Update the API file with twig loader hook

--- twigshim.api.php
+++ twigshim.api.php
@@ -6,13 +6,22 @@
  */
 
 /**
- * Initializes the Twigshim Twig environment.
+ * Initializes the Twigshim Twig loader.
  *
- * @param $twig
- *   The Twig environment that is being initialized.
+ * @param Twig_Loader_Filesystem $loader
+ *   The Twig loader that is being initialized.
  */
-function hook_twigshim_environment($twig) {
+function hook_twigshim_loader(Twig_Loader_Filesystem $loader) {
   // Add the @mymodulecomponents namespace.
   $path = drupal_get_path('module', 'mymodule');
-  $twig->addPath($path . '/components, 'mymodulecomponents');
+  $twig->addPath($path . '/components', 'mymodulecomponents');
+}
+
+/**
+ * Initializes the Twigshim Twig environment.
+ *
+ * @param Twig_Environment $twig
+ *   The Twig environment that is being initialized.
+ */
+function hook_twigshim_environment(Twig_Environment $twig) {
 }
